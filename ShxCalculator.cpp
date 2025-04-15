5fc949f8259d84a81f55180256567189882c95e1
GHMaksym <ghmaksym@gmail.com>
2025-04-15 16:12:12 +0300

    Hotfix: FuncA sums first 3 elements regardless of n

--- ShxCalculator.cpp
+++ ShxCalculator.cpp
@@ -1,5 +1,12 @@
 #include "ShxCalculator.h"
+#include <cmath>
 
 double ShxCalculator::FuncA(int n) {
-    return 0.0;
+    double result = 0.0;
+    int fixedN = 3;
+    for (int i = 0; i < fixedN; i++) {
+        double term = pow(1.0, 2 * i + 1) / tgamma(2 * i + 2);
+        result += term;
+    }
+    return result;
 }
