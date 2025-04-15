8c87ef14405e44b7644cc4eac12dce5c1718e1b1
GHMaksym <ghmaksym@gmail.com>
2025-04-15 16:09:48 +0300

    Implemented FuncA: calculates sum of first n elements of sinh(x) series with x=1

--- ShxCalculator.cpp
+++ ShxCalculator.cpp
@@ -1,5 +1,11 @@
 #include "ShxCalculator.h"
+#include <cmath>
 
 double ShxCalculator::FuncA(int n) {
-    return 0.0;
+    double result = 0.0;
+    for (int i = 0; i < n; i++) {
+        double term = pow(1.0, 2 * i + 1) / tgamma(2 * i + 2);
+        result += term;
+    }
+    return result;
 }
