ffb767527e1ebf185340587e130512d4b4330269
GHMaksym <ghmaksym@gmail.com>
2025-04-23 22:09:35 +0300

    update FuncA to calc sh(x) for arbitrary x

--- ShxCalculator.cpp
+++ ShxCalculator.cpp
@@ -1,10 +1,10 @@
 #include "ShxCalculator.h"
 #include <cmath>
 
-double ShxCalculator::FuncA(int n) {
+double ShxCalculator::FuncA(int n, double x) {
     double result = 0.0;
     for (int i = 0; i < n; i++) {
-        double term = pow(1.0, 2 * i + 1) / tgamma(2 * i + 2);
+        double term = pow(x, 2 * i + 1) / tgamma(2 * i + 2);
         result += term;
     }
     return result;
